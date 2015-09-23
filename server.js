574a099161c59a49838d8059729229043cd40898
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2015-09-23 15:42:35 +0100

    second

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
+// imports and variables.
 var express = require('express'),
 		app = express(),
+	 exphbs = require('express-handlebars'),
 	   port = (process.env.PORT || 3000);
 
+// setting up a rendering engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+// then assigning that engine to be the view engine.
+app.set('view engine', 'handlebars');
+
+// doing something on every request.
 app.use(function (req, res, next) {
-  console.log("got a request");
+  // console.log("got a request");
+  console.log(process.env.NODE_ENV);
   next();
 });
 
+// dealing with the request and responding.
 app.get(/.*/,function(req, res)
 {
-	res.send('Fishing is great! Isn\'t it');
+	res.render('home');
 });
 
+// set it off listening.
 app.listen(port);
 console.log("\n"+'Listening on port ' + port + "\n");
